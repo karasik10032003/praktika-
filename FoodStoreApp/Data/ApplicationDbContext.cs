4c5f356ec0dc40b0f9f942ceadca1620d022d602
Карасик <@DESKTOP-NS53AEF>
2022-11-21 21:27:13 +0300

    Добавьте файлы проекта.

---
+++ FoodStoreApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using FoodStoreApp.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace FoodStoreApp.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(
+            DbContextOptions<ApplicationDbContext> options): base(options)
+        {
+        }
+
+        public DbSet<Category> Category { get; set; }
+        public DbSet<Manager> Manager { get; set; }
+        public DbSet<Product> Product { get; set; }
+    }
+}
---
+++ FoodStoreApp/Migrations/20221101100548_ass.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FoodStoreApp.Migrations
+{
+    public partial class ass : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
